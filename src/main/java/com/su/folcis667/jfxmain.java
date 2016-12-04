4168da65ecd5914cfad5d85b3c46ca241f01af0c
go away <spam@spam.spam>
2016-12-04 15:44:01 -0500

    show matches
    added a list view to show the valid matches

--- src/main/java/com/su/folcis667/jfxmain.java
+++ src/main/java/com/su/folcis667/jfxmain.java
@@ -27,14 +27,19 @@ package com.su.folcis667;
 
 import com.su.folcis667.match3fx.StateViewController;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -47,43 +52,42 @@ public class jfxmain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-//        content.getChildren().setAll();
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
         FXMLLoader fxml_loader = new FXMLLoader(getClass().getResource("/match3fx/StateView.fxml"));
         GridPane state_view = null;
         try {
             state_view = (GridPane) fxml_loader.load();
-//       Scene myScene = new Scene(myPane);
-//       primaryStage.setScene(myScene);
-//       primaryStage.show();
         } catch (IOException ex) {
             Logger.getLogger(jfxmain.class.getName()).log(Level.SEVERE, null, ex);
             return;
         }
         StateViewController state_view_controller = (StateViewController) fxml_loader.getController();
-
-//        GridPane state_view = new GridPane();
-//        state_view.getChildren().setAll(FXMLLoader.load("StateView.fxml"));
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(state_view, 800, 650);
-
+        
+        ListView<String> swaps = new ListView<>();
+        ObservableList<String> items =FXCollections.observableArrayList();
+        swaps.setItems(items);
+        
+        GridPane main_view = new GridPane();
+        main_view.add(state_view, 0, 0);
+        main_view.add(swaps, 0, 1);
+        Scene scene = new Scene(main_view, 800, 650);
         primaryStage.setTitle("Match 3 Puzzle Solver");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-        Match3Game asdf = new Match3Game(5, 5, 4);
+
+        Match3Game asdf = new Match3Game(5, 5, 3);
         state_view_controller.refresh(asdf.mCells);
+        
+        ArrayList<Match3Game.MatchingPair> pairs = asdf.GetMatchableCells();
+        for(Match3Game.MatchingPair pair : pairs){
+            String match = "";
+            match += pair.mLeft.c() + " match: ";
+            match += "(" + pair.mLeft.x() + ", " + pair.mLeft.y() + ")";
+            match += " and (" + pair.mRight.x() + ", " + pair.mRight.y() + ")";
+            items.add(match);
+        }
+        if (pairs.isEmpty()){
+            items.add("NO MATCHES POSSIBLE FOR THIS STATE!");
+        }
     }
 
     /**
